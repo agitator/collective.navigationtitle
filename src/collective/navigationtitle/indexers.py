e4ef92f6f4a8891531330388a875148601cfd427
Peter Holzer <hpeter@agitator.com>
2015-05-19 19:05:07 +0200

    update indexer

--- src/collective/navigationtitle/indexers.py
+++ src/collective/navigationtitle/indexers.py
@@ -1,11 +1,11 @@
-from .interfaces import IShortTitleNavtreeStrategy
+from .behaviors.navigation_title import INavigationTitle
 from plone.indexer import indexer
 
 
-@indexer(IShortTitleNavtreeStrategy)
+@indexer(INavigationTitle)
 def short_title(obj):
     import ipdb; ipdb.set_trace()
-    dx = IShortTitleNavtreeStrategy(obj, None)
+    dx = INavigationTitle(obj, None)
     if dx is None:
         return None
     return dx.navigation_title
