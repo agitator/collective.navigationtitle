51c800f8adf3f6b5ce23b8d5bb2b0d6489179f81
Peter Holzer <hpeter@agitator.com>
2015-05-20 14:31:42 +0200

    zca fun

--- src/collective/navigationtitle/navigation.py
+++ src/collective/navigationtitle/navigation.py
@@ -2,25 +2,33 @@
 from .interfaces import IShortTitleNavtreeStrategy
 from plone.app.layout.navigation.navtree import NavtreeStrategyBase
 from zope.interface import implementer
+from zope.component import adapter
 from plone.app.layout.navigation.interfaces import INavtreeStrategy
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.portlets.portlets.navigation import INavigationPortlet
+from Products.CMFCore.interfaces import IContentish
+
+from OFS.interfaces import IItem
+
 
 class BrainWrapper(object):
 
     def __init__(self, brain):
         self.brain = brain
 
-    def __getattr__(self, name):
-        import ipdb; ipdb.set_trace()
-        if name == 'Title':
-            return self.brain.short_title
-        else:
-            return getattr(self, brain)
+#    def __getattr__(self, name):
+#        import ipdb; ipdb.set_trace()
+#        if name == 'Title':
+#            return self.brain.short_title
+#        else:
+#            return getattr(self, brain)
 
 
 @implementer(INavtreeStrategy)
+@adapter(INavigationRoot, INavigationPortlet)
 class ShortTitleNavtreeStrategy(NavtreeStrategyBase):
 
     def decoratorFactory(self, node):
         import ipdb; ipdb.set_trace()
-        node['item'] = BrainWrapper(node['item'])
-        return node
+#        node['item'] = BrainWrapper(node['item'])
+#        return node
