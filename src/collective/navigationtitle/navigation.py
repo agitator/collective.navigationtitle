f38b815096804a364aa726cacc086178633557a8
Peter Holzer <hpeter@agitator.com>
2015-05-20 17:47:29 +0200

    fixes

--- src/collective/navigationtitle/navigation.py
+++ src/collective/navigationtitle/navigation.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-from .interfaces import IShortTitleNavtreeStrategy
-from plone.app.layout.navigation.navtree import NavtreeStrategyBase
+from plone.app.portlets.portlets.navigation import NavtreeStrategy
 from zope.interface import implementer
 from zope.component import adapter
 from plone.app.layout.navigation.interfaces import INavtreeStrategy
-from plone.app.layout.navigation.interfaces import INavigationRoot
 from plone.app.portlets.portlets.navigation import INavigationPortlet
 from Products.CMFCore.interfaces import IContentish
-
-from OFS.interfaces import IItem
 
 
 class BrainWrapper(object):
@@ -16,19 +12,17 @@ class BrainWrapper(object):
     def __init__(self, brain):
         self.brain = brain
 
-#    def __getattr__(self, name):
-#        import ipdb; ipdb.set_trace()
-#        if name == 'Title':
-#            return self.brain.short_title
-#        else:
-#            return getattr(self, brain)
+    def __getattr__(self, name):
+        if name == 'Title':
+            return self.brain.short_title
+        else:
+            return getattr(self.brain, name)
 
 
 @implementer(INavtreeStrategy)
 @adapter(IContentish, INavigationPortlet)
-class ShortTitleNavtreeStrategy(NavtreeStrategyBase):
+class ShortTitleNavtreeStrategy(NavtreeStrategy):
 
     def decoratorFactory(self, node):
-        import ipdb; ipdb.set_trace()
-#        node['item'] = BrainWrapper(node['item'])
-#        return node
+        node['item'] = BrainWrapper(node['item'])
+        return node
